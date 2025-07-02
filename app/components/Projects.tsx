d02eff0ee717a2b4ab7532e23950be0be4653597
abdulhaseebshah <haseebwe5@gmail.com>
2025-07-02 12:03:40 +0530

    added contents in prject section

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import {
+  SiAxios,
+  SiCss3,
+  SiHtml5,
+  SiJavascript,
   SiNextdotjs,
+  SiReact,
   SiReactquery,
   SiSupabase,
   SiTailwindcss,
@@ -13,18 +18,18 @@ import { DirectionAwareHover } from "@/components/ui/direction-aware-hover";
 const Projects = () => {
   const projects = [
     {
-      title: "E-Learning Platform (onlydevkh)",
-      tech: [SiNextdotjs, SiReactquery, SiSupabase, SiTailwindcss],
+      title: "Real-Time Weather Forecast App",
+      tech: [SiReact, SiAxios, SiHtml5, SiTailwindcss],
       Link: "http://localhost:3000",
       cover: "/project-1.jpg",
       background: "bg-indigo-500",
     },
     {
-      title: "E-Learning Platform (onlydevkh)",
-      tech: [SiNextdotjs, SiReactquery, SiSupabase, SiTailwindcss],
+      title: "Movie App",
+      tech: [SiReact, SiAxios, SiHtml5, SiCss3],
       Link: "http://localhost:3000",
-      cover: "/project-1.jpg",
-      background: "bg-violet-800",
+      cover: "/project-2.jpg",
+      background: "bg-indigo-500",
     },
   ];
 
@@ -32,7 +37,7 @@ const Projects = () => {
     <div className="py-10 p-5 sm:p-0">
       <Title
         text="Projects 🎨"
-        className="flex flex-col justify-center items-center rotate-6"
+        className="flex flex-col justify-center items-center -rotate-6"
       />
 
       <div className="grid grid-cols-1 sm:grid-cols-2 pt-20 gap-5">
@@ -45,11 +50,7 @@ const Projects = () => {
                   className="w-full space-y-5 cursor-pointer"
                 >
                   <div className="space-y-5">
-                    <h1
-                      className="text-2xl font-bold"
-                    >
-                      {project.title}
-                    </h1>
+                    <h1 className="text-2xl font-bold">{project.title}</h1>
                     <div className="flex items-center gap-2">
                       {project.tech.map((Icon, index) => {
                         return <Icon key={index} className="w-8 h-8" />;
